87360ed9b24a1897faa70130dde88a8baed6d8e5
motascientist <mota_scientist@outlook.com>
2024-01-13 13:30:18 -0300

    Changing streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,58 +10,40 @@ df['day'] = pd.to_datetime(df['day'])
 df = df.sort_values(by=['day', 'price'])
 df.dropna(inplace=True)
 
-
 # Obter a taxa de câmbio do Euro para Real usando o Yahoo Finance
 eur_to_brl = yf.download('EURBRL=X')['Close'][-1]
-
-
 
 # Streamlit settings
 st.set_page_config(page_title='Real Estate App', page_icon=':house:', layout='wide')
 
-# Sidebar for district selection
-selected_district = st.sidebar.selectbox('Select a District:', df['district'].unique())
+# Show the 10 best prices based on m2-€ column within a specified price interval
+st.subheader('Top 10 Prices based on m²')
 
-# Filtrar os preços com base no distrito selecionado
-prices_in_selected_district = df[df['district'] == selected_district]['price'].unique()
+# Manually input the price interval
+min_price = st.text_input('Enter Min Price:', df['price'].min())
+max_price = st.text_input('Enter Max Price:', df['price'].max())
 
-# Sidebar para seleção de preço, adaptado aos preços do distrito selecionado
-selected_price = st.sidebar.selectbox('Select a Price:', sorted(prices_in_selected_district))
+# Convert input to float
+min_price = float(min_price) if min_price else df['price'].min()
+max_price = float(max_price) if max_price else df['price'].max()
 
-# Sidebar para date selection with reversed order
-selected_date = st.sidebar.selectbox('Select a Date:', sorted(df['day'].unique(), reverse=True))
+# Filter data within the specified price interval
+filtered_data = df[(df['price'] >= min_price) & (df['price'] <= max_price)]
 
-# Filter the DataFrame to find all rows corresponding to the selected price, date, and district
-selected_rows = df[(df['price'] == selected_price) & (df['day'] == selected_date) & (df['district'] == selected_district)]
+# Display the top 10 prices based on m2-€
+top_10_prices = filtered_data.nsmallest(10, 'm2-€')
 
+for i, row in top_10_prices.iterrows():
+    st.write(f'Description: {row["description"]}')
+    st.write(f'Number of Rooms: {row["rooms"]}')
+    st.write(f'Square Meters: {row["m2"]}')
+    st.write(f'Bathrooms: {row["bathrooms"]}')
+    st.write(f'Price m² €: {round(row["m2-€"],2)}')
+    st.write(f'Price €: {row["price"]}')
+    st.write(f'Price R$: {round(row["price"] * eur_to_brl, 2)}')
+    st.write(f'Link: {row["links"]}')
+    st.markdown('---')  # Add a divider line between information for different houses
 
-# Display information based on the selected price and date
-if not selected_rows.empty:
-    st.subheader('Selected House Information')
-    for index, row in selected_rows.iterrows():
-        st.write(f'Date: {row["day"].strftime("%Y-%m-%d")}')
-        st.write(f'Description: {row["description"]}')
-        st.write(f'Number of Rooms: {row["rooms"]}')
-        st.write(f'Square Meters: {row["m2"]}')
-        st.write(f'Bathrooms: {row["bathrooms"]}')
-        st.write(f'Price €: {row["price"]}')
-        st.write(f'Price R$: {round(row["price"]*eur_to_brl,2)}')
-        st.write(f'Link: {row["links"]}')
-        st.markdown('---')  # Add a divider line between information for different houses
-
-    # Calcula e exibe o preço médio e desvio padrão com base nos dados da data mais recente
-    avg_price_recent_date = df[df['day'] == df['day'].max()]['price'].mean()
-    #std_dev_price_recent_date = df[df['day'] == df['day'].max()]['price'].std()
-    #max_price = df[df['day'] == df['day'].max()]['price'].max()
-    #min_price = df[df['day'] == df['day'].max()]['price'].min()
-
-
-    st.sidebar.write(f'Average Price €: {avg_price_recent_date:.2f}')
-    #st.sidebar.write(f'Standard Deviation €: {std_dev_price_recent_date:.2f}')
-    #st.sidebar.write(f'Max Price €: {max_price:.2f}')
-    #st.sidebar.write(f'Lower Price €: {min_price:.2f}')
-
-
-
-else:
+if top_10_prices.empty:
     st.warning('No data found for the selected price.')
+
